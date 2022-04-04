1d497d3b5a938ab4614ae81dd2685f1b9b3a279b
kolobabka <belov.vv@phystech.edu>
2022-04-04 22:55:56 +0300

    Add profiling

---
+++ BitonicSort/sources/time.cpp
@@ -0,0 +1,61 @@
+#include "bitsort.hpp"
+#include "common.hpp"
+#include <algorithm>
+
+int main () try {
+
+    std::vector<int> sequence;
+    inputSequence<int> (sequence);
+    std::cout << "\t\tSize of input array: " << sequence.size() << std::endl;
+
+    std::cout << "\t\t\t###GPU-SORT###" << std::endl;
+#ifdef KERNEL
+
+    std::cout << KERNEL << std::endl;
+    BitonicSort::GPUSortApp<int> GPUapp(KERNEL);
+#else
+    BitonicSort::GPUSortApp<int> GPUapp("../kernels/kernel.cl");
+#endif
+
+    BitonicSort::TotalTime seconds = GPUapp.sort(sequence.begin(), sequence.end());
+
+    std::cout << "CPU time: " << seconds.CPUTime << "ms\n";
+    std::cout << "GPU time: " << seconds.GPUTime << "ms\n";
+
+    std::cout << "\t\t\t###STD-SORT###" << std::endl;
+        
+
+    auto start = std::chrono::high_resolution_clock::now();
+    std::sort(sequence.begin(), sequence.end());
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "STD::Sort time: " << 
+    std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\n";  
+    return 0;
+
+}
+catch (cl::BuildError &err) {
+
+    std::cerr << "OCL BUILD ERROR: " << err.err() << ":" << err.what()
+            << std::endl;
+    std::cerr << "-- Log --\n";
+    for (auto e : err.getBuildLog())
+    std::cerr << e.second;
+    std::cerr << "-- End log --\n";
+    return -1;
+} 
+catch (cl::Error &err) {
+
+    std::cerr << "OCL ERROR: " << err.err() << ":" << err.what() << std::endl;
+    return -1;
+} 
+catch (std::runtime_error &err) {
+
+    std::cerr << "RUNTIME ERROR: " << err.what() << std::endl;
+    return -1;
+} 
+catch (...) {
+
+    std::cerr << "UNKNOWN ERROR\n";
+    return -1;
+}
